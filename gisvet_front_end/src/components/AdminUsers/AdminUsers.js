ffbbcf1a9a794779f5d359dc681622fa91c5a40c
EmersonUrielVargas <emerson.vargas@uptc.edu.co>
2022-10-29 16:56:43 -0500

    Se añade Boton a campos de la Tabla

--- gisvet_front_end/src/components/AdminUsers/AdminUsers.js
+++ gisvet_front_end/src/components/AdminUsers/AdminUsers.js
@@ -6,15 +6,34 @@ import icon_Search from  "./images/Icon_Search.png"
 import icon_Filter from "./images/Icon_Filter.png"
 import SettingsAdminUser from "../SettingsAdminUser/index";
 import {useUsersAdmin} from "../../hooks/useAdminUsers";
+import { Modal } from "../../components/Modal/Index"; 
+import AddUser from "../../components/AddUser";
+import ShowUser from "../../components/ShowUser";
 
 
 
 export default function AdminUser(){
     const {loading, users,headers} = useUsersAdmin()
+    const [showModal, setShowModal] = useState(false)
+    const [childModal, setchildModal] = useState(<></>)
+
+    const showUserMenu = async(identifier) =>{
+        setShowModal(true)
+        setchildModal(<ShowUser
+            onClose={handleCloseModal} 
+            onSubmit={handleCloseModal} 
+           />) 
+    }
+
+    const handleCloseModal = ()=>{
+        setShowModal(false)
+    }
 
 
 
-    return (
+
+
+    return (<>
         <div className="general-users">
             <h1>Gestión de Usuarios</h1>
                 <div className="table-users">
@@ -30,10 +49,18 @@ export default function AdminUser(){
                         </div>
                     </div>
 
-                <Table headers={headers} data={users}/>  
+                <Table headers={headers} 
+                        data={users}
+                        keyName={'document'}
+                        actionItem={showUserMenu}
+                        />  
                 <SettingsAdminUser /> 
             </div>
         </div>
+        {showModal && <Modal>{childModal}</Modal>
+                        
+        }
+    </>
     )
 
 }
