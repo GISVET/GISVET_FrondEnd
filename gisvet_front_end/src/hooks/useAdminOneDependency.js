15c5dc35608f6f4143b2ebd7a8324583b099c10e
Frankma17 <frankin.canon@uptc.edu.co>
2022-11-01 20:37:08 -0500

    GISVET-80 Agregar detalle de dependencias y modificar dependencias

--- gisvet_front_end/src/hooks/useAdminOneDependency.js
+++ gisvet_front_end/src/hooks/useAdminOneDependency.js
@@ -1,7 +1,8 @@
-import { useContext,useState,useEffect} from "react";
+import { useContext, useCallback, useState, useEffect } from "react";
 import userContext from "../context/UserContext";
 import { dependenciesAdmin } from "../constants/headersTables";
 import getDependencyDetails from "../services/getDependencyDetail";
+import updateDependency from "../services/updateDependency";
 import { gender } from "../constants/constants";
 
 export function useAdminOneDependency(id_dependencie) {
@@ -11,12 +12,11 @@ export function useAdminOneDependency(id_dependencie) {
   const [loading, setLoading] = useState();
   let errorMessage = "";
 
-  useEffect(()=>{
-    askDependency()
-},[])
+  useEffect(() => {
+    askDependency();
+  }, []);
 
-
-  const askDependency = ()=>{
+  const askDependency = () => {
     setLoading(true);
     getDependencyDetails({ jwt, id_dependencie })
       .then((res) => {
@@ -26,29 +26,53 @@ export function useAdminOneDependency(id_dependencie) {
           setLoading(false);
           errorMessage = res.message;
           setDependency(res);
-          
         }
       })
       .catch((err) => {});
-  }
+  };
 
-  function formatListUsers(){
-    let dataFormated = []
+  const updateDependencyFunction = useCallback(
+    ( id_dependencie, dependencie_name ) => {
+      setLoading(true);
+      console.log("Antes de enviar el endpoint")
+      console.log(`En el hookk Llega el id ${id_dependencie}  y el name ${dependencie_name}`)
+      updateDependency({ jwt, id_dependencie, dependencie_name })
+        .then((res) => {
+          if (res.message === "") {
+            setLoading(false);
+          } else {
+            setLoading(false);
+            errorMessage = res.message;
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+    [setLoading]
+  );
+
+  function formatListUsers() {
+    let dataFormated = [];
     const dataPersons = dependency.person_dependencies;
-    dataPersons.map((person)=>{
-        const gender_person = gender.find(element => element.id === person.persons.GENDER);  
+    dataPersons.map((person) => {
+      const gender_person = gender.find(
+        (element) => element.id === person.persons.GENDER
+      );
+      const profesionalIdValidation  = (person.persons.PROFESSIONAL_ID==='null')?"No aplica":person.persons.PROFESSIONAL_ID;     
 
-        let personData={
-            id_person:person.persons.ID_PERSON,
-            full_name:person.persons.FULL_NAME,
-            document:`${person.persons.DOCUMENT_TYPE} ${person.persons.DOCUMENT}`,
-            gender_person:gender_person.name,
-            profesional_id:person.persons.PROFESSIONAL_ID,
-        }
-        dataFormated.push(personData);
-    })
+
+      let personData = {
+        id_person: person.persons.ID_PERSON,
+        full_name: person.persons.FULL_NAME,
+        document: `${person.persons.DOCUMENT_TYPE} ${person.persons.DOCUMENT}`,
+        gender_person: gender_person.name,
+        profesional_id: profesionalIdValidation,
+      };
+      dataFormated.push(personData);
+    });
     setPersons(dataFormated);
-}
+  }
 
   return {
     loading,
@@ -57,6 +81,7 @@ export function useAdminOneDependency(id_dependencie) {
     persons,
     headers: dependenciesAdmin,
     askDependency,
+    updateDependencyFunction,
     formatListUsers,
   };
 }
