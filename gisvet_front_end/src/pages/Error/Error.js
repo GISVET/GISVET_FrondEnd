e6a05776977c5d75b79aec70595af057f872fe9a
EmersonUrielVargas <71224875+EmersonUrielVargas@users.noreply.github.com>
2022-12-11 21:48:26 -0500

    add link to login in error page

--- gisvet_front_end/src/pages/Error/Error.js
+++ gisvet_front_end/src/pages/Error/Error.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useEffect} from "react";
 import styles from "./styles.module.css";
 import banner from "./images/Background_404.png";
 
+import useLocationWouter from "wouter/use-location";
+import {useLocation, Route, Link } from "wouter"
+
+
+
 
 export default function Error() {
+  const [location] = useLocationWouter();
+  const [,navigate] = useLocation()
+
+  useEffect(()=>{
+      console.log(location)
+  },[location])
 
   return (
     <div className={styles.general_admin}>
       <div className={styles.principal_error}>
           <img src={banner} alt="Banner de error"></img>
-          <h2>Ops ! Lo sentimos pero en este momento no estamos disponibles</h2>
+          <h2>Ops ! Lo sentimos pero en este momento la pagina solicitada no está en uso</h2>
+          <a onClick={() => navigate("/")}>Ir a iniciar sesión</a>
       </div>
     </div>
   );
